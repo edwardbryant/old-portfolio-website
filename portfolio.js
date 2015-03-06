4fa11bfd3db6a4dc83b42b506915e3ec3059e4a4
edwardbryant <edward.bryant@gmail.com>
2015-03-06 16:55:02 -0600

    fix responsive bugs, and basic jquery

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 
+    getBlogPosts();
+
     $('#about-expand').on('click', function() {
         $('#about-extra').toggle(500, function() {
             var more = 'More <i class="fa fa-angle-down"></i>';
@@ -9,8 +11,33 @@ $(document).ready(function(){
         });
     })
 
-
-
-
-
 });
+
+var getBlogPosts = function() {
+
+    // TO DO - replace hand-coded data with API calls to WP-API.
+
+    var data = {
+        1: {
+            "title": "Example Post Title Number One",
+            "date": "February 20, 2015",
+            "text": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. deserunt mollitia animi, id est laborum et dolorum fuga."
+        },
+        2: {
+            "title": "Another Example Post Title, The Sequel",
+            "date": "March 1, 2015",
+            "text": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
+        }
+    }
+
+    // populate data
+
+    $('#post1').find('.title').html(data[1]["title"]);
+    $('#post1').find('.date').html("&bull; " + data[1]["date"] + " &bull;");
+    $('#post1').find('.text').html(data[1]["text"] + ' <a href="#">More &hellip;</a>');
+
+    $('#post2').find('.title').html(data[2]["title"]);
+    $('#post2').find('.date').html("&bull; " + data[2]["date"] + " &bull;");
+    $('#post2').find('.text').html(data[2]["text"] + ' <a href="#">More &hellip;</a>');
+
+}; 
