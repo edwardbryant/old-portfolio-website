ef73abfaca51bb40444992d24596a4db4e77083a
edwardbryant <edward.bryant@gmail.com>
2015-03-08 15:16:37 -0500

    nav changes

--- portfolio.js
+++ portfolio.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
 
     $('#btn-nav').on('click', function() {
         $.playSound('sfx/sound2');
-
-        
-
         $('nav').find('ul').toggle(600);
         return false;
     })
